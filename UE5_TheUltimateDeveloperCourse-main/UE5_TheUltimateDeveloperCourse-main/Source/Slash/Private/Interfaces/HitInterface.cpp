138ceed54ef9c8202cf97c985a6b553fe619e0f7
Ke Wang <kwang655@usc.edu>
2024-01-05 09:52:27 -0800

    Delete UE5_TheUltimateDeveloperCourse-main/UE5_TheUltimateDeveloperCourse-main/Source directory
    Deleted file

--- UE5_TheUltimateDeveloperCourse-main/UE5_TheUltimateDeveloperCourse-main/Source/Slash/Private/Interfaces/HitInterface.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Interfaces/HitInterface.h"
-
-// Add default functionality here for any IHitInterface functions that are not pure virtual.
